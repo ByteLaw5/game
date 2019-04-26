eae648c55c218bc5053cad43423f5fde9889356a
aradgal <31285856+aradgal@users.noreply.github.com>
2019-04-26 19:58:27 +0300

    Added Source Files

---
+++ com/game/util/ID.java
@@ -0,0 +1,21 @@
+package com.game.util;
+
+/**
+ * Each ID has it's own integer id. That means you can spawn an enemy using the number 3.
+ */
+public enum ID {
+	Player(1),
+	Block(2),
+	Enemy(3),
+	Bullet(4);
+	
+	private int id;
+	
+	private ID(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+}
