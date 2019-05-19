ac561448371a90f18857b04e29c2aa5e2c8606e2
CreatorVilius <viliuskubilius416@gmail.com>
2019-05-19 15:15:39 +0300

    Vibration fix

--- src/com/game/Camera.java
+++ src/com/game/Camera.java
@@ -13,7 +13,7 @@ public class Camera implements IHasPlace {
 	
 	public void tick(Player player) {
 		this.setX(-player.getX() + (GameBase.WIDTH / 2));
-		this.setY(-player.getY() + (GameBase.HEIGHT / 2));
+		this.setY((float)(int)(-player.getY() + (GameBase.HEIGHT / 2)));
 	}
 	
 	@Override
